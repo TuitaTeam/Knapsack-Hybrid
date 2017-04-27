28d78d6d0b1503926eb3f3eba867a0d9ed73b473
Marc Sánchez <sanchezfauste@gmail.com>
2017-04-27 16:34:07 +0200

    Adding scripts to run tests on moore cluster

---
+++ run_all.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+THREADS="2 4 8 16 32"
+PROBLEMS=$(find testbed -name "*" -type f)
+SERIAL="./knapsackBB_serial"
+PARALLEL="./knapsackBB_mpi"
+THREADS_FILE="threads.csv"
+
+if [ ! -d "output" ]; then
+    mkdir output
+fi
+if [ ! -d "errors" ]; then
+    mkdir errors
+fi
+
+echo $THREADS > $THREADS_FILE
+
+for PROBLEM in $PROBLEMS; do
+    THREAD=1
+    PROBLEM_NAME=$(head -n 1 $PROBLEM | awk '{print $1 "_" $2}')
+    qsub -N "P.$PROBLEM_NAME.$THREAD" -pe mpich $THREAD run-simple-mpi.sh $SERIAL $PROBLEM
+    for THREAD in $THREADS; do
+        qsub -N "P.$PROBLEM_NAME.$THREAD" -pe mpich $THREAD run-simple-mpi.sh $PARALLEL $PROBLEM
+    done
+done
